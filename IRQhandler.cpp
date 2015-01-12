e3e287e68888806caf29a7a5d071e93c57a82bcc
micheleliscio <liscio.m90@gmail.com>
2015-01-12 16:40:26 +0100

    adds syscfg->exticr1 line and various comments

--- IRQhandler.cpp
+++ IRQhandler.cpp
@@ -31,6 +31,7 @@ void __attribute__((naked)) EXTI0_IRQHandler()
 
 void __attribute__((used)) EXTI0HandlerImpl()
 {
+    /* is set by HW when the selected edge event arrives on the external interrupt line. It's cleared writing 1 */
     EXTI->PR=EXTI_PR_PR0;
     
     if(waiting==0) return;
@@ -44,10 +45,14 @@ void IRQhandler::configureAccInterrupt()
 {
 	//create interrupt on rising edge of the button bit (when the button is pressed)
     int1Signal::mode(Mode::INPUT);
-    EXTI->IMR |= EXTI_IMR_MR0;
-    EXTI->RTSR |= EXTI_RTSR_TR0;
+    SYSCFG->EXTICR[0] = SYSCFG_EXTICR1_EXTI0_PE; //use PE0 for EXTI_Line0
+    EXTI->IMR |= EXTI_IMR_MR0; //interrupt request from line 0 is not masked
+    EXTI->RTSR |= EXTI_RTSR_TR0; //rising trigger enabled
+    
+    /* enables a device-specific interrupt in the NVIC interrupt controller; 
+    EXTI0_IRQn = 6 : EXTI Line0 Interrupt = STM32 specific Interrupt Number */
     NVIC_EnableIRQ(EXTI0_IRQn);
-    NVIC_SetPriority(EXTI0_IRQn,15);  
+    NVIC_SetPriority(EXTI0_IRQn,15); //low priority
 }
 
 void IRQhandler::waitForInt1()
